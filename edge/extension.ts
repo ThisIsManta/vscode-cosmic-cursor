57404f4e7ed796cb2e4dd5b2c873c15c0dbec2fa
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2017-11-10 22:43:18 +0700

    Added "openRecent" and "openSimilar" commands

--- edge/extension.ts
+++ edge/extension.ts
@@ -4,6 +4,8 @@ import * as os from 'os'
 import * as _ from 'lodash'
 import * as vscode from 'vscode'
 
+let openingEditors: Array<vscode.TextEditor> = []
+
 export function activate(context: vscode.ExtensionContext) {
     // let rootConfig: RootConfigurations
 
@@ -31,7 +33,56 @@ export function activate(context: vscode.ExtensionContext) {
             select: false,
         })
     }))
+
+    vscode.window.onDidChangeActiveTextEditor(activeEditor => {
+        if (openingEditors.indexOf(activeEditor) >= 0) {
+            openingEditors.splice(openingEditors.indexOf(activeEditor), 1)
+        }
+        openingEditors.unshift(activeEditor)
+    })
+
+    vscode.workspace.onDidCloseTextDocument(closingDocument => {
+        openingEditors = openingEditors.filter(editor => editor.document !== closingDocument)
+    })
+
+    context.subscriptions.push(vscode.commands.registerCommand('cosmicCursor.openRecent', () => {
+        const recentEditor = _.last(openingEditors.slice(0, 2))
+        if (recentEditor) {
+            vscode.window.showTextDocument(recentEditor.document, recentEditor.viewColumn)
+        }
+    }))
+
+    context.subscriptions.push(vscode.commands.registerCommand('cosmicCursor.openSimilar', async () => {
+        if (!vscode.window.activeTextEditor) {
+            return null
+        }
+
+        const fileLink = vscode.window.activeTextEditor.document.uri
+        const rootLink = vscode.workspace.getWorkspaceFolder(fileLink)
+        if (!rootLink) {
+            return null
+        }
+
+        const filePath = vscode.window.activeTextEditor.document.uri.fsPath
+        const fileName = fp.basename(filePath)
+        if (fileName.startsWith('.') || fileName.includes('.') === false) {
+            return null
+        }
+
+        const relaPath = filePath.substring(rootLink.uri.fsPath.length)
+        const dirxPath = fp.dirname(relaPath)
+        const lazyName = fileName.replace(/\..+/, '')
+        const lazyPath = (dirxPath + '/' + lazyName).replace(/\\/g, '/').replace(/^\//, '')
+
+        const fileList = await vscode.workspace.findFiles(lazyPath + '.*')
+        const selxRank = fileList.findIndex(nextLink => nextLink.fsPath === fileLink.fsPath)
+        if (selxRank >= 0) {
+            const nextLink = fileList.concat(fileList)[selxRank + 1]
+            vscode.window.showTextDocument(nextLink)
+        }
+    }))
 }
 
 export function deactivate() {
+    openingEditors = null
 }
