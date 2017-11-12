00bb914c9b83ca0733db1b4fed4481ad07acc68b
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2017-11-12 23:49:55 +0700

    Removed "openRecent", "openSimilar", and "openPackage" commands

--- edge/extension.ts
+++ edge/extension.ts
@@ -4,15 +4,13 @@ import * as vscode from 'vscode'
 import { moveCursor } from './moveCursor'
 import { selectCursorByBlock, shrinkCursorByBlock } from './selectCursorByBlock'
 import { moveOrSelectCursorByWordLeft, moveOrSelectCursorByWordRight } from './moveOrSelectCursorByWord'
-import { openSimilar, openPackage } from './files'
-
-let openingEditors: Array<vscode.TextEditor> = []
-let cursorPairHistory: Array<vscode.Selection> = []
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('cosmicCursor.cursorUp', moveCursor(-1)))
     context.subscriptions.push(vscode.commands.registerCommand('cosmicCursor.cursorDown', moveCursor(+1)))
 
+    let cursorPairHistory: Array<vscode.Selection> = []
+    
     context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(e => {
         if (e.kind !== vscode.TextEditorSelectionChangeKind.Command) {
             cursorPairHistory.splice(0, cursorPairHistory.length)
@@ -41,30 +39,4 @@ export function activate(context: vscode.ExtensionContext) {
         })
     }))
 
-    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(activeEditor => {
-        if (openingEditors.indexOf(activeEditor) >= 0) {
-            openingEditors.splice(openingEditors.indexOf(activeEditor), 1)
-        }
-        openingEditors.unshift(activeEditor)
-    }))
-
-    context.subscriptions.push(vscode.workspace.onDidCloseTextDocument(closingDocument => {
-        openingEditors = openingEditors.filter(editor => editor.document !== closingDocument)
-    }))
-
-    context.subscriptions.push(vscode.commands.registerCommand('cosmicCursor.openRecent', () => {
-        const recentEditor = _.last(openingEditors.slice(0, 2))
-        if (recentEditor) {
-            vscode.window.showTextDocument(recentEditor.document, recentEditor.viewColumn)
-        }
-    }))
-
-    context.subscriptions.push(vscode.commands.registerCommand('cosmicCursor.openSimilar', openSimilar))
-
-    context.subscriptions.push(vscode.commands.registerCommand('cosmicCursor.openPackage', openPackage))
-}
-
-export function deactivate() {
-    openingEditors = null
-    cursorPairHistory = null
 }
