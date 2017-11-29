a3bae55d99341794ea2c1d4b16b6b87ba6103ff6
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2017-11-29 12:00:26 +0700

    Amended "cursorWordLeft" command so that it stops at punctuations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,10 @@
+### 2.0.0
+- Amended `cosmicCursor.cursorWordLeft` and `cosmicCursor.cursorWordRight` commands so that they snap to `()`, `{}`, `[]`, `''`, `""`, and ` `` `.
+
 ### 1.0.0
 - Renamed `cosmicCursor.cursorUpSelect` to `cosmicCursor.smartSelect.expand` command.
 - Renamed `cosmicCursor.cursorDownSelect` to `cosmicCursor.smartSelect.shrink` command.
-- Amended `ctrl+left` and `ctrl+right` commands so that they snap to the first/end of its current line.
+- Amended `cosmicCursor.cursorWordLeft` and `cosmicCursor.cursorWordRight` commands so that they snap to the first/end of its current line.
 - Removed `shift+backspace` keybinding.
 
 ### 0.0.1
--- edge/moveOrSelectCursorByWord.ts
+++ edge/moveOrSelectCursorByWord.ts
@@ -9,6 +9,80 @@ const setCursorOrSelection = (lineRank: number, charRank: number, select: boolea
 	)
 }
 
+const rsPairs = [
+	'\\(\\s*\\)',
+	'\\{\\s*\\}',
+	'\\[\\s*\\]',
+	'\'\\s*\'',
+	'"\\s*"',
+	'`\\s*`',
+]
+const rePairs = RegExp(rsPairs.join('|'))
+
+// The following code is copied from https://github.com/lodash/lodash/blob/4.17.4/lodash.js#L206
+const rsAstralRange = '\\ud800-\\udfff',
+	rsComboMarksRange = '\\u0300-\\u036f',
+	reComboHalfMarksRange = '\\ufe20-\\ufe2f',
+	rsComboSymbolsRange = '\\u20d0-\\u20ff',
+	rsComboRange = rsComboMarksRange + reComboHalfMarksRange + rsComboSymbolsRange,
+	rsDingbatRange = '\\u2700-\\u27bf',
+	rsLowerRange = 'a-z\\xdf-\\xf6\\xf8-\\xff',
+	rsMathOpRange = '\\xac\\xb1\\xd7\\xf7',
+	rsNonCharRange = '\\x00-\\x2f\\x3a-\\x40\\x5b-\\x60\\x7b-\\xbf',
+	rsPunctuationRange = '\\u2000-\\u206f',
+	rsSpaceRange = ' \\t\\x0b\\f\\xa0\\ufeff\\n\\r\\u2028\\u2029\\u1680\\u180e\\u2000\\u2001\\u2002\\u2003\\u2004\\u2005\\u2006\\u2007\\u2008\\u2009\\u200a\\u202f\\u205f\\u3000',
+	rsUpperRange = 'A-Z\\xc0-\\xd6\\xd8-\\xde',
+	rsVarRange = '\\ufe0e\\ufe0f',
+	rsBreakRange = rsMathOpRange + rsNonCharRange + rsPunctuationRange + rsSpaceRange
+
+// The following code is copied from https://github.com/lodash/lodash/blob/4.17.4/lodash.js#L222
+const rsApos = "['\u2019]",
+	rsAstral = '[' + rsAstralRange + ']',
+	rsBreak = '[' + rsBreakRange + ']',
+	rsCombo = '[' + rsComboRange + ']',
+	rsDigits = '\\d+',
+	rsDingbat = '[' + rsDingbatRange + ']',
+	rsLower = '[' + rsLowerRange + ']',
+	rsMisc = '[^' + rsAstralRange + rsBreakRange + rsDigits + rsDingbatRange + rsLowerRange + rsUpperRange + ']',
+	rsFitz = '\\ud83c[\\udffb-\\udfff]',
+	rsModifier = '(?:' + rsCombo + '|' + rsFitz + ')',
+	rsNonAstral = '[^' + rsAstralRange + ']',
+	rsRegional = '(?:\\ud83c[\\udde6-\\uddff]){2}',
+	rsSurrPair = '[\\ud800-\\udbff][\\udc00-\\udfff]',
+	rsUpper = '[' + rsUpperRange + ']',
+	rsZWJ = '\\u200d'
+
+// The following code is copied from https://github.com/lodash/lodash/blob/4.17.4/lodash.js#L239
+const rsMiscLower = '(?:' + rsLower + '|' + rsMisc + ')',
+	rsMiscUpper = '(?:' + rsUpper + '|' + rsMisc + ')',
+	rsOptContrLower = '(?:' + rsApos + '(?:d|ll|m|re|s|t|ve))?',
+	rsOptContrUpper = '(?:' + rsApos + '(?:D|LL|M|RE|S|T|VE))?',
+	reOptMod = rsModifier + '?',
+	rsOptVar = '[' + rsVarRange + ']?',
+	rsOptJoin = '(?:' + rsZWJ + '(?:' + [rsNonAstral, rsRegional, rsSurrPair].join('|') + ')' + rsOptVar + reOptMod + ')*',
+	rsOrdLower = '\\d*(?:(?:1st|2nd|3rd|(?![123])\\dth)\\b)',
+	rsOrdUpper = '\\d*(?:(?:1ST|2ND|3RD|(?![123])\\dTH)\\b)',
+	rsSeq = rsOptVar + reOptMod + rsOptJoin,
+	rsEmoji = '(?:' + [rsDingbat, rsRegional, rsSurrPair].join('|') + ')' + rsSeq,
+	rsSymbol = '(?:' + [rsNonAstral + rsCombo + '?', rsCombo, rsRegional, rsSurrPair, rsAstral].join('|') + ')'
+
+// The following code is copied and slightly modified from https://github.com/lodash/lodash/blob/4.17.4/lodash.js#L265
+const reUnicodeWord = RegExp([
+	rsUpper + '?' + rsLower + '+' + rsOptContrLower + '(?=' + [rsBreak, rsUpper, '$'].join('|') + ')',
+	rsMiscUpper + '+' + rsOptContrUpper + '(?=' + [rsBreak, rsUpper + rsMiscLower, '$'].join('|') + ')',
+	rsUpper + '?' + rsMiscLower + '+' + rsOptContrLower,
+	rsUpper + '+' + rsOptContrUpper,
+	rsOrdUpper,
+	rsOrdLower,
+	rsDigits,
+	rsEmoji,
+	...rsPairs, // Note that this is my improvisation
+].join('|'), 'g')
+
+const splitWordsOrPairs = (text: string, excludePairs: boolean) => {
+	return (text.match(reUnicodeWord) || []).filter(word => !(excludePairs && rePairs.test(word)))
+}
+
 export const moveOrSelectCursorByWordLeft = (select: boolean) => async () => {
 	const editor = vscode.window.activeTextEditor
 	let lineRank = editor.selection.active.line
@@ -17,9 +91,13 @@ export const moveOrSelectCursorByWordLeft = (select: boolean) => async () => {
 		editor.selection.active,
 	))
 
-	const wordText = _.last(_.words(lineText))
+	const wordText = _.last(splitWordsOrPairs(lineText, select))
 	if (wordText) {
-		const wordRank = lineText.lastIndexOf(wordText)
+		let wordRank = lineText.lastIndexOf(wordText)
+		if (rePairs.test(wordText)) {
+			wordRank = wordRank - wordText.length + 1
+		}
+
 		if (wordRank + wordText.length === editor.selection.active.character) {
 			return setCursorOrSelection(lineRank, wordRank, select)
 
@@ -32,7 +110,7 @@ export const moveOrSelectCursorByWordLeft = (select: boolean) => async () => {
 	}
 
 	while (true) {
-		const wordList = _.words(lineText)
+		const wordList = splitWordsOrPairs(lineText, select)
 		if (wordList.length === 0) {
 			if (lineRank === 0) {
 				break
@@ -45,7 +123,11 @@ export const moveOrSelectCursorByWordLeft = (select: boolean) => async () => {
 		}
 
 		const wordText = _.last(wordList)
-		const wordRank = lineText.lastIndexOf(wordText) + wordText.length
+		let wordRank = lineText.lastIndexOf(wordText) + wordText.length
+		if (rePairs.test(wordText)) {
+			wordRank = wordRank - wordText.length + 1
+		}
+
 		return setCursorOrSelection(lineRank, wordRank, select)
 	}
 }
@@ -58,9 +140,13 @@ export const moveOrSelectCursorByWordRight = (select: boolean) => async () => {
 		editor.document.lineAt(editor.selection.active.line).range.end,
 	))
 
-	const wordText = _.first(_.words(lineText))
+	const wordText = _.first(splitWordsOrPairs(lineText, select))
 	if (wordText) {
-		const wordRank = editor.selection.active.character + lineText.indexOf(wordText)
+		let wordRank = editor.selection.active.character + lineText.indexOf(wordText)
+		if (rePairs.test(wordText)) {
+			wordRank = wordRank + 1
+		}
+
 		if (wordRank === editor.selection.active.character) {
 			return setCursorOrSelection(lineRank, wordRank + wordText.length, select)
 
@@ -73,7 +159,7 @@ export const moveOrSelectCursorByWordRight = (select: boolean) => async () => {
 	}
 
 	while (true) {
-		const wordList = _.words(lineText)
+		const wordList = splitWordsOrPairs(lineText, select)
 		if (wordList.length === 0) {
 			if (lineRank === editor.document.lineCount - 1) {
 				break
@@ -86,7 +172,11 @@ export const moveOrSelectCursorByWordRight = (select: boolean) => async () => {
 		}
 
 		const wordText = _.first(wordList)
-		const wordRank = lineText.indexOf(wordText)
+		let wordRank = lineText.indexOf(wordText)
+		if (rePairs.test(wordText)) {
+			wordRank = wordRank + 1
+		}
+
 		return setCursorOrSelection(lineRank, wordRank, select)
 	}
 }
